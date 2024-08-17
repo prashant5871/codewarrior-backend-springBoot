b8852f880fdc18f36583bd4d1973499c83ca7040
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-08-17 15:38:58 +0530

    initlized spring boot project

---
+++ codewarrior/src/main/java/com/codewarrior/CodewarriorApplication.java
@@ -0,0 +1,13 @@
+package com.codewarrior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewarriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewarriorApplication.class, args);
+	}
+
+}
---
+++ codewarrior/src/test/java/com/codewarrior/CodewarriorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewarrior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewarriorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
